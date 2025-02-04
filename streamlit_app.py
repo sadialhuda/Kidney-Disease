43bbbc69959ebd5eb50377b0448a85f8db7d892f
Sadi Al huda <67068215+sadialhuda@users.noreply.github.com>
2025-02-04 22:06:49 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,10 @@
-# import streamlit as st
-# import joblib
-# import pandas as pd
-
-# # Load the trained model
-# model = joblib.load("KNN_model.pkl")  # Make sure the model file is in the same directory
-
-# # Function to make predictions
-# def predict(input_data):
-#     prediction = model.predict([input_data])
-#     return prediction
-
-# # Streamlit app code
-# st.title('Streamlit App with Model')
-
-# # User input (for example, numeric input)
-# input_data = st.text_input("Enter input data:")
-
-# # Convert input data to the format required by the model
-# # Assuming the model expects a list of features (e.g., a list of numbers)
-# if input_data:
-#     input_data = [float(i) for i in input_data.split(",")]  # Assuming comma-separated input
-#     prediction = predict(input_data)
-#     st.write(f"Prediction: {prediction}")
 
 import streamlit as st
 import joblib
 import numpy as np
 
 # Load the trained model
-model = joblib.load("KNN_model.pkl")  # Ensure the model file is in the same directory
+model = joblib.load("best_rf_model.pkl")  # Ensure the model file is in the same directory
 
 # Function to make predictions
 def predict(input_data):
@@ -36,10 +12,10 @@ def predict(input_data):
     return prediction[0]  # Return the single prediction value
 
 # Streamlit app code
-st.title("Prostate Cancer Recurrence Prediction")
+st.title("Kidney Disease Prediction")
 
 st.markdown("""
-This app predicts if cancer may occur again based on user-provided data. 
+This app predicts ckd disease. 
 Fill in the fields below and click **Predict**.
 """)
 
